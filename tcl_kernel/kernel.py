f3661f44d25be6738e3935b0e0358ad43ad15bb1
ryanpepper <rp20g15@soton.ac.uk>
2016-01-27 22:37:06 +0000

    First attempt at kernel

---
+++ tcl_kernel/kernel.py
@@ -0,0 +1,29 @@
+from ipykernel.kernelbase import Kernel
+import tkinter
+__version__ = '0.1'
+
+class TclKernel(Kernel):
+    implementation = 'tcl_kernel'
+    implementation_version = __version__
+    language_info = {'name': 'bash',
+                     'codemirror_mode': 'shell',
+                     'mimetype': 'text/x-script.tcl',
+                     'file_extension': '.tcl'}
+
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        try:
+            self.tcl = tkinter.Tcl()
+        
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        
+        output = self.tcl(code.rstrip())
+        
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': code}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok', 'execution_count': self.execution_count,
+                    'payload': [], 'user_expressions': {}}
+
