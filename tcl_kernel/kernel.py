1efb93b8445504454b3666c5caa338d14fb3566c
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-02-02 11:47:49 +0000

    Added empty string

--- tcl_kernel/kernel.py
+++ tcl_kernel/kernel.py
@@ -5,7 +5,6 @@ import sys
 import tempfile
 import platform
 import ctypes
-from subprocess import Popen, PIPE
 
 OS = platform.platform()
 libc = ctypes.CDLL(None)
@@ -15,7 +14,7 @@ elif 'Linux' in OS:
     c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
 
 else:
-    print("""Your OS is not currently compatible with this Kernel \n 
+    print("""Your OS is not currently compatible with this Kernel \n
              Please make an issue here: \n
              https://github.com/ryanpepper/tcl_kernel
              """)
@@ -65,32 +64,49 @@ class TclKernel(Kernel):
 
     implementation = 'tcl_kernel'
     implementation_version = __version__
-    language_info = {'name': 'Tcl',
-                     'codemirror_mode': 'Tcl',
-                     'mimetype': 'text/x-tcl',
-                     'file_extension': '.tcl'}
-    banner = "Tcl Kernel"
+    language_info = {"name": "Tcl",
+                     "codemirror_mode": {
+                         "version": 2,
+                         "name": "text/x-tcl"
+                     },
+                     "mimetype": "text/x-tcl",
+                     "file_extension": ".tcl"}
+
+    _banner = "Tcl Kernel"
+
+    @property
+    def banner(self):
+        if self._banner is None:
+            banner_undecoded = self._run_code('puts [info patchlevel]')
+            self._banner = banner_undecoded.decode('utf-8')
+        return self._banner
 
     def __init__(self, **kwargs):
         Kernel.__init__(self, **kwargs)
         self.tcl = Tkinter.Tcl()
         self.execution_count = 0
 
-    def do_execute(self, code, silent, store_history=True,
-                   user_expressions=None, allow_stdin=False):
-        stderr_output = None
+    def _run_code(self, code, decodestr='utf-8'):
         temp_stdout_stream = io.BytesIO()
-        last = ''
+        stderr_output = None
+        returnval = ''
         with stdout_redirector(temp_stdout_stream):
             try:
-                last = self.tcl.eval(code)
+                returnval = self.tcl.eval(code)
             except Tkinter.TclError as scripterr:
-                stderr_output = "Tcl Error: " + scripterr.args[0]
+                stderr_output = scripterr.args[0]
         output = temp_stdout_stream.getvalue()
         temp_stdout_stream.close()
+        return (output.decode(decodestr) + returnval,
+                stderr_output)
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        output, stderr_output = self._run_code(code)
 
         if not silent:
-            stream_content = {'name': 'stdout', 'text': output.decode('utf-8') + last}
+            stream_content = {
+                'name': 'stdout', 'text': output}
             self.send_response(self.iopub_socket, 'stream', stream_content)
             if stderr_output:
                 stream_content = {
